3d36cec1666dd6fe50aed05246a599487b32194b
GC2023 <91566024+GithubCoder2021@users.noreply.github.com>
2023-03-16 10:26:01 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function tab(tab){
+
+document.getElementById("games").style.display = "none";
+document.getElementById("other").style.display = "none";
+document.getElementById(tab).style.display = "inline-block";
+}
+
+tab('games');
