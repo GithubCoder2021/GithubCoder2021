bba8181bd56c8f9a8db4ddb64c46692949932f4f
GC2023 <91566024+GithubCoder2021@users.noreply.github.com>
2023-03-24 12:23:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function tab(tab){
-
+document.getElementById("side").style.display = "none";
 document.getElementById("games").style.display = "none";
 document.getElementById("other").style.display = "none";
 document.getElementById(tab).style.display = "inline-block";
